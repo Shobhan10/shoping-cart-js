679c980a00da732b7817b2c874ca73def8d5070c
Shobhan Sundar Goutam <shobhangoutam@gmail.com>
2023-01-21 10:22:50 +0530

    localhost setup

--- src/main.js
+++ src/main.js
@@ -1,11 +1,21 @@
 const shop = document.getElementById("shop");
 
+let cartItems = [];
+const previousItems = localStorage.getItem("cart");
+if (!previousItems) {
+  cartItems = [];
+} else {
+  cartItems = JSON.parse(previousItems);
+}
+
 generateItems();
 
 function generateItems() {
   shop.innerHTML = shopItemsData
     .map((item) => {
       const { id, name, price, desc, img } = item;
+      let cartItem = cartItems.find((item) => item.id === id);
+
       return `
         <div id="product-id-${id}" class="item">
             <img src=${img} alt="${name}" width="220" />
@@ -16,7 +26,9 @@ function generateItems() {
                 <h2>$ ${price}</h2>
                 <div class="buttons">
                   <i onclick="decrement(${id})" class="bi bi-dash-lg"></i>
-                  <div id="${id}" class="quantity">0</div>
+                    <div id=${id} class="quantity">${
+        cartItem ? cartItem.value : 0
+      }</div>
                   <i onclick="increment(${id})" class="bi bi-plus-lg"></i>
                 </div>
               </div>
@@ -51,9 +63,24 @@ function generateItems() {
 }
 
 function increment(id) {
-  let value = Number(id.textContent);
+  let quantity = id;
+  let value = Number(quantity.textContent);
   value += 1;
-  id.textContent = value;
+  quantity.textContent = value;
+
+  cartItems.find((item) => {
+    if (item.id === quantity.id) {
+      item.value = value;
+    }
+    localStorage.setItem("cart", JSON.stringify(cartItems));
+  });
+
+  let item = cartItems.find((item) => item.id === quantity.id);
+
+  if (!item) {
+    cartItems.push({ id: quantity.id, value });
+    localStorage.setItem("cart", JSON.stringify(cartItems));
+  }
 }
 
 const decrement = (id) => {
